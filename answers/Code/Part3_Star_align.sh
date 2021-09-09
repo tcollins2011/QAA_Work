ae4accb0541d048d67632776d85a4f383f959727
Tyler Collins <tcollin2@uoregon.edu>
2021-09-08 23:52:30 -0700

    updated star_align

--- answers/Code/Part3_Star_align.sh
+++ answers/Code/Part3_Star_align.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=bgmp
 #SBATCH --partition=bgmp
 #SBATCH --cpus-per-task=8
-
+#SBATCH --time=5:00:00
 conda activate bgmp_py39
 
 /usr/bin/time -v STAR --runThreadN 8 --runMode alignReads \
@@ -11,7 +11,7 @@ conda activate bgmp_py39
  --outSAMunmapped Within KeepPairs \
  --alignIntronMax 1000000 --alignMatesGapMax 1000000 \
  --readFilesCommand zcat \
- --readFilesIn /projects/bgmp/shared/Bi621/dre_WT_ovar12_R1.qtrim.fq.gz /projects/bgmp/shared/Bi621/dre_WT_ovar12_R2.qtrim.fq.gz \
+ --readFilesIn /projects/bgmp/tcollin2/bioinfo/Bi623/PS/QAA_Work/genome/adapter_work/7_2E_fox__R1_trimmed_adptrmv.fq.gz  /projects/bgmp/tcollin2/bioinfo/Bi623/PS/QAA_Work/genome/adapter_work/7_2E_fox__R2_trimmed_adptrmv.fq.gz \
  --genomeDir /projects/bgmp/tcollin2/bioinfo/Bi623/PS/QAA_Work/genome/Mouse.GRCm39.dna.ens104.STAR_2.7.9a \
  --outFileNamePrefix Mouse
 
